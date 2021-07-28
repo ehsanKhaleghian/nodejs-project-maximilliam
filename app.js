f1d2622f0fe304ce1967dae78ba253b4b4287dda
Ehsan khaleghian <e.khaleghian@gmail.com>
2021-07-28 07:35:23 +0430

    module export

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
 const http = require("http");
-const fs = require("fs");
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    console.log("URL::::", url);
-    const method = req.method;
-    if (url === "/") {
-        res.setHeader("Content-Type", "text/html");
-        res.write("<html>");
-        res.write("<head><title>INPUT MESSAGE</title><head>");
-        res.write(
-            "<body><form action='message' method='POST'><input type='text' name='message'><button type='submit'>SEND</button></input></form></body>"
-        );
-        res.write("</html>");
-        return res.end();
-    }
-    if (url === "/message" && method === "POST") {
-        fs.writeFileSync("message.txt", "DUMMY");
-        ``;
-        res.statusCode = 302;
-        res.setHeader("location", "/");
-        return res.end();
-    }
-});
+const routse = require("./routes");
+
+const server = http.createServer(routse);
 server.listen(7777);
