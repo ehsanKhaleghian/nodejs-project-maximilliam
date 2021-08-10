6ab3b24542018c62e0aaf52f71176d408ab5a06d
Ehasn Khaleghian <e.khaleghian@rooberah.co>
2021-08-10 10:42:36 +0430

    nodemon

--- S/app.js
+++ S/app.js
@@ -1,7 +1,5 @@
 const http = require("http");
-
-const express = require("express");
-const routse = require("./routes");
+const routse = require("../routes");
 
 const server = http.createServer(routse);
 server.listen(7777);
