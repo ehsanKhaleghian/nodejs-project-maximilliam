61a7e5d6691a61291b2a98217e00adf96f5ded42
Ehasn Khaleghian <e.khaleghian@rooberah.co>
2021-08-16 09:37:13 +0430

    Refactor

--- S/app.js
+++
@@ -1,5 +0,0 @@
-const http = require("http");
-const routse = require("../routes");
-
-const server = http.createServer(routse);
-server.listen(7777);
--- assignments/Assignment1/app.js
+++
@@ -1,5 +0,0 @@
-const http = require("http");
-const route = require("./routes.js");
-
-const server = http.createServer(route);
-server.listen(7000);
