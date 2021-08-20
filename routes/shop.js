b69ba704ba793ed9998ed639ba1ac7971651abf5
Ehasn Khaleghian <e.khaleghian@rooberah.co>
2021-08-20 10:14:01 +0430

    Send HTML Files

--- routes/shop.js
+++ routes/shop.js
@@ -4,9 +4,12 @@ const express = require("express");
 
 const rootDir = require("../utils/path");
 
+const adminData = require("../routes/admin");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+    console.log("Products:::", adminData.products);
     res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
